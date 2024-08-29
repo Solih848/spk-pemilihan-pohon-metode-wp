0f20bba2ff5ac2998af8caa6d032cb1f7fef0c21
Solih848 <solihudin848@gmail.com>
2024-08-30 06:42:14 +0700

    Create index.php

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            font-family: Segoe UI Historic, Segoe UI, Helvetica, Arial, sans-serif
+        }
+
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .card {
+            background: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            margin: 20px auto;
+            max-width: 1200px;
+            width: 100%;
+            padding: 0;
+            overflow: hidden;
+        }
+
+        .card-header {
+            background-color: #111111;
+            color: #fff;
+            padding: 15px;
+            text-align: center;
+        }
+        .card-body {
+            padding: 20px;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="card">
+        <div class="card-header">
+            <h2>Selamat Datang</h2>
+        </div>
+        <div class="card-body">
+            <h3>Cara Penggunaan:</h3>
+            <ol>
+                <li>Kunjungi halaman kriteria untuk menambahkan data kriteria.</li>
+                <li>Tambahkan data alternatif pada halaman alternatif.</li>
+                <li>Lihat hasil perhitungan pada halaman hasil.</li>
+            </ol>
+        </div>
+    </div>
+</body>
+
+</html>
