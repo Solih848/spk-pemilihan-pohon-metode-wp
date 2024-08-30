38826353d5ba0e24b241122c70b7b64c2fd7a3f7
Solih848 <solihudin848@gmail.com>
2024-08-30 13:55:22 +0700

    Update pohon_db.sql

--- database/pohon_db.sql
+++ database/pohon_db.sql
@@ -1,1 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 30 Agu 2024 pada 08.51
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.0.30
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pohon_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kriteria`
+--
+
+CREATE TABLE `kriteria` (
+  `id` int(11) NOT NULL,
+  `nama_kriteria` varchar(255) NOT NULL,
+  `bobot` float NOT NULL,
+  `kategori` enum('benefit','cost') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `kriteria`
+--
+
+INSERT INTO `kriteria` (`id`, `nama_kriteria`, `bobot`, `kategori`) VALUES
+(2, 'Curah Hujan ', 0.5, 'benefit'),
+(9, 'Ketinggian', 0.2, 'benefit'),
+(11, 'Air', 0.3, 'benefit'),
+(12, 'Kesulitan Akses', 0.3, 'cost');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `nilai_kriteria`
+--
+
+CREATE TABLE `nilai_kriteria` (
+  `id` int(11) NOT NULL,
+  `pohon_id` int(11) NOT NULL,
+  `kriteria_id` int(11) NOT NULL,
+  `nilai` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `nilai_kriteria`
+--
+
+INSERT INTO `nilai_kriteria` (`id`, `pohon_id`, `kriteria_id`, `nilai`) VALUES
+(36, 9, 2, 7),
+(37, 9, 9, 5),
+(38, 9, 11, 3),
+(39, 9, 12, 2),
+(40, 12, 2, 4),
+(41, 12, 9, 4),
+(42, 12, 11, 2),
+(43, 12, 12, 4),
+(44, 13, 2, 5),
+(45, 13, 9, 4),
+(46, 13, 11, 1),
+(47, 13, 12, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pohon`
+--
+
+CREATE TABLE `pohon` (
+  `id` int(11) NOT NULL,
+  `nama_pohon` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pohon`
+--
+
+INSERT INTO `pohon` (`id`, `nama_pohon`) VALUES
+(9, 'Kamboja'),
+(12, 'Mangga'),
+(13, 'Pisang');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`) VALUES
+(2, 'admin@gmail.com', '11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `kriteria`
+--
+ALTER TABLE `kriteria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `nilai_kriteria`
+--
+ALTER TABLE `nilai_kriteria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `pohon_id` (`pohon_id`),
+  ADD KEY `kriteria_id` (`kriteria_id`);
+
+--
+-- Indeks untuk tabel `pohon`
+--
+ALTER TABLE `pohon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `kriteria`
+--
+ALTER TABLE `kriteria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT untuk tabel `nilai_kriteria`
+--
+ALTER TABLE `nilai_kriteria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- AUTO_INCREMENT untuk tabel `pohon`
+--
+ALTER TABLE `pohon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `nilai_kriteria`
+--
+ALTER TABLE `nilai_kriteria`
+  ADD CONSTRAINT `nilai_kriteria_ibfk_1` FOREIGN KEY (`pohon_id`) REFERENCES `pohon` (`id`),
+  ADD CONSTRAINT `nilai_kriteria_ibfk_2` FOREIGN KEY (`kriteria_id`) REFERENCES `kriteria` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
