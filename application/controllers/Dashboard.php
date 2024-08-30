920da255cd8169bf70d108172aaefd9b747d5c3c
Solih848 <solihudin848@gmail.com>
2024-08-30 13:45:51 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -1,10 +1,21 @@
 <?php
 class Dashboard extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('session');
+        if (!$this->session->userdata('user')) {
+            redirect('auth'); // Redirect ke halaman login jika belum login
+        }
+    }
+
     public function index()
     {
+        $data['email'] = $this->session->userdata('user')->email;
         $this->load->view('ly/header');
-        $this->load->view('dashboard/index');
+        $this->load->view('dashboard/index', $data);
         $this->load->view('ly/footer');
     }
 }
